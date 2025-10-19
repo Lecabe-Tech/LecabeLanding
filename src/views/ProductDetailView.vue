0343d7d734ccd9234706573bc462c2687b4bc0d4
Patrick Grilanda <patrick.grilanda26@gmail.com>
2025-10-19 10:45:07 -0300

    fix: Remove unused computed import from ProductDetailView

--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, onMounted, watch } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { useProducts } from '@/composables/useProducts'
